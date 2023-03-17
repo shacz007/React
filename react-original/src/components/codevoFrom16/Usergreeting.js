fa6fafa4525442544c06ca110f64ee51cd1b5494
shacz007 <sachinsiddrampur@gmail.com>
2023-03-17 10:04:58 +0530

    ss

--- react-original/src/components/codevoFrom16/Usergreeting.js
+++ react-original/src/components/codevoFrom16/Usergreeting.js
@@ -1,10 +1,53 @@
 import React, { Component } from "react";
 
 class Usergreeting extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            isLoggeedIn : false
+        }
+    }
     render(){
-        return(
-            <div>HEllo sachin</div>
-        )
+        // shortcircuit operator
+
+        return this.state.isLoggeedIn && <h1>welome sachin</h1>
+
+
+        /* // using ternary operator
+            return (
+                this.state.isLoggeedIn? (<h1>welcome sachin</h1>):
+                (<h1>welcome guest</h1>)
+            ) */
+
+            
+        /* // using element variables
+        let message;
+        if(this.state.isLoggeedIn){
+            message=<h1>welcome sachin</h1>
+        } else{
+            message=<h1>welcome guest</h1>
+        }
+        return message; */
+
+
+        /* // using if/else condition
+        if(this.state.isLoggeedIn){
+            return <div>
+                <h1>welcome sachin</h1>
+            </div>
+        }else{
+            return <div>
+            <h1>welcome guest</h1>
+        </div>
+        } */
+        // return(
+        //     <div>
+        //         <h1>HEllo sachin</h1>
+        //         <h1>Welcome guest</h1>
+        //         </div>
+
+        // )
     }
 }
 export default Usergreeting
