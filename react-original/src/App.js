4809375299c0409a3b84e283fff7eeaa218b3096
shacz007 <sachinsiddrampur@gmail.com>
2023-03-22 16:09:32 +0530

    ll

--- react-original/src/App.js
+++ react-original/src/App.js
@@ -20,14 +20,16 @@ import PersonList from "./components/codevoFrom16/List render/PersonList";
 import PersonList1 from "./components/codevoFrom16/List render/PersonList1";
 import Normal from "./components/prostack/bind/Normal";
 import WithBind from "./components/prostack/bind/WithBind";
+import Loginform from "./components/prostack/Form Handling/Loginform"
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Loginform/>
                 {/* <WithBind/> */}
                 {/* <Normal/> */}
-                <PersonList1/>
+                {/* <PersonList1/> */}
                 {/* <PersonList/> */}
                 {/* <NameList/> */}
                 {/* <Usergreeting/> */}
