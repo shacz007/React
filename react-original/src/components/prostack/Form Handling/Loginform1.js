c19083719c450bc65e3ea6df360cd47933e60606
shacz007 <sachinsiddrampur@gmail.com>
2023-03-22 17:07:22 +0530

    zfdf

--- react-original/src/components/prostack/Form Handling/Loginform1.js
+++ react-original/src/components/prostack/Form Handling/Loginform1.js
@@ -4,19 +4,34 @@ class Loginform extends Component {
     constructor(props){
         super(props)
         this.state = {
-            
+            email :"",
+            password: ""
         }
     }
+
+   emailHandler = (event)=>{
+        this.setState({email : event.target.value})
+    }
+
+    passwordHandler = (event)=>{
+      this.setState({password :event.target.value})
+    }
+    submitHandler = (event)=>{
+      event.preventDefault();
+    }
+    
   render() {
     return (
       <div>
             <React.Fragment>
               <h2>Login details</h2>
-              <form>
+              <pre>{JSON.stringify(this.state)}</pre>
+              <form onSubmit={this.submitHandler}>
                 <label>Email</label>
-                <input  placeholder="ENTER email" type="text" onChange={this.emailHandler} />
+                <input  placeholder="ENTER email" type="email" onChange={this.emailHandler} />
                 <label>password</label>
                 <input  placeholder="ENTER email" type="password" onChange={this.passwordHandler} />
+                <button>submit</button>
               </form>
             </React.Fragment>
       </div>
