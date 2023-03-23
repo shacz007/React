c5a42c8c2cc7f34eaf4fc84713ea3089035584ab
shacz007 <sachinsiddrampur@gmail.com>
2023-03-23 15:38:10 +0530

    ok

--- react-original/src/components/prostack/AxiosWlifecycle/DigitalClock.js
+++ react-original/src/components/prostack/AxiosWlifecycle/DigitalClock.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
 export class DigitalClock extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         currentTime : new Date().toLocaleTimeString()
+      }
+    }
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({
+                currentTime : new Date().toLocaleTimeString()
+            })
+    },1000)
+}
   render() {
     return (
-      <div>DigitalClock</div>
+      <div>
+        <h1 style={{fontsize : 50}}>DigitalClock</h1>
+        <h1 style={{fontSize: 300}}>{this.state.currentTime}</h1>
+        </div>
     )
   }
 }
