140e2cd925babea0d6539c83784357a29b8f9e3d
shacz007 <sachinsiddrampur@gmail.com>
2023-03-23 15:28:06 +0530

    dds

---
+++ react-original/src/components/prostack/AxiosWlifecycle/DigitalClock.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class DigitalClock extends Component {
+  render() {
+    return (
+      <div>DigitalClock</div>
+    )
+  }
+}
+
+export default DigitalClock
