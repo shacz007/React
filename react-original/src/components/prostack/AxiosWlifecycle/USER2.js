6a55f23d83d0abce204dd1b4fa45cb7fc28d853a
shacz007 <sachinsiddrampur@gmail.com>
2023-03-23 15:19:40 +0530

    ok

--- react-original/src/components/prostack/AxiosWlifecycle/USER2.js
+++ react-original/src/components/prostack/AxiosWlifecycle/USER2.js
@@ -1,9 +1,43 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
  class USER2 extends Component {
+    constructor(){
+        super()
+        this.state={
+            user : []
+        }
+    }
+
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/albums")
+        .then((resp)=>{
+            this.setState({
+                user : resp.data
+            })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+
   render() {
     return (
-      <div>USER2</div>
+      <div>
+            <pre>{JSON.stringify(this.state)}</pre>
+            {
+                this.state.user.length > 0 ? <>
+                {
+                    this.state.user.map((users)=>{
+                        return <tr>
+                            <td>{users.id}</td>
+                            <td>{users.title}</td>
+                        </tr>
+                    })
+                }
+                </> : null
+            }
+      </div>
     )
   }
 }
