7dac917c4abf31150ca923673dd6ead7391a9ddf
shacz007 <sachinsiddrampur@gmail.com>
2023-03-24 13:55:50 +0530

    ok

--- react-original/src/App.js
+++ react-original/src/App.js
@@ -8,45 +8,27 @@ import ClassClick from "./components/codevo/ClassClick";
 import Eventbind from "./components/codevo/Eventbind";
 import InsertIMG from "./components/codevo/InsertIMG";
 import InsertIMGcls from "./components/codevo/InsertIMGcls";
-import FuncPropCom from "./components/prostack/FuncPropCom";
-import FuncPropCom1 from "./components/prostack/FuncPropCom1"
-import ClassPROP from "./components/prostack/ClassPROP";
-import States from "./components/prostack/States";
+
 import Parentcomponent from "./components/codevo/Parentcomponent";
 import Usergreeting from "./components/codevoFrom16/conditional render/Usergreeting";
 import NameList from "./components/codevoFrom16/List render/NameList";
 import PersonList from "./components/codevoFrom16/List render/PersonList";
 import PersonList1 from "./components/codevoFrom16/List render/PersonList1";
-import Normal from "./components/prostack/bind/Normal";
-import WithBind from "./components/prostack/bind/WithBind";
-import Loginform from "./components/prostack/Form Handling/Loginform1"
-import Loginform2 from "./components/prostack/Form Handling/Loginform2";
-import Loginform3 from "./components/prostack/Form Handling/Loginform3";
-import USER1 from "./components/prostack/AxiosWlifecycle/USER1";
-import USER2 from "./components/prostack/AxiosWlifecycle/USER2";
-import DigitalClock from "./components/prostack/AxiosWlifecycle/DigitalClock";
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <DigitalClock/>
-                {/* <USER2/> */}
-                {/* <USER1/> */}
-                {/* <Loginform3/> */}
-                {/* <Loginform2></Loginform2> */}
-                {/* <Loginform1/> */}
-                {/* <WithBind/> */}
-                {/* <Normal/> */}
+                
                 {/* <PersonList1/> */}
                 {/* <PersonList/> */}
                 {/* <NameList/> */}
                 {/* <Usergreeting/> */}
                 {/* <Parentcomponent></Parentcomponent> */}
                 {/* <States/> */}
-                {/* <FuncPropCom/> */}
-                {/* <FuncPropCom1/> */}
-                {/* <ClassPROP></ClassPROP> */}
+                
                 {/* <InsertIMG/>     */}
                 {/* <InsertIMGcls/> */}
                 {/* <Counter/> */}
--- react_prostack/src/App.js
+++ react_prostack/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Normal from "./components/prostack/bind/Normal";
+import WithBind from "./components/prostack/bind/WithBind";
+import Loginform from "./components/prostack/Form Handling/Loginform1"
+import Loginform2 from "./components/prostack/Form Handling/Loginform2";
+import Loginform3 from "./components/prostack/Form Handling/Loginform3";
+import USER1 from "./components/prostack/AxiosWlifecycle/USER1";
+import USER2 from "./components/prostack/AxiosWlifecycle/USER2";
+import DigitalClock from "./components/prostack/AxiosWlifecycle/DigitalClock";
+import FuncPropCom from "./components/prostack/FuncPropCom";
+import FuncPropCom1 from "./components/prostack/FuncPropCom1"
+import ClassPROP from "./components/prostack/ClassPROP";
+import States from "./components/prostack/States";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <>
+     {/* <USER2/> */}
+                {/* <USER1/> */}
+                {/* <Loginform3/> */}
+                {/* <Loginform2></Loginform2> */}
+                {/* <Loginform1/> */}
+                {/* <WithBind/> */}
+                {/* <Normal/> */}
+                {/* <FuncPropCom/> */}
+                {/* <FuncPropCom1/> */}
+                {/* <ClassPROP></ClassPROP> */}
+    </>
+  )
+  }
+export default App
