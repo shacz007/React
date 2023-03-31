52f2afda05376c67dd205966d6e02ba8e01f1820
shacz007 <sachinsiddrampur@gmail.com>
2023-03-31 11:14:50 +0530

    ok

--- react_UserApp/src/components/Nav.jsx
+++ react_UserApp/src/components/Nav.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 function Nav(){
     return <div>
-        <nav>
-            <Link to="#">USER APP</Link>
-                <div>
-                    <li><Link to="index">HOME</Link></li>
-                    <li><Link to="user">USER</Link></li>
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <Link className="navbar-brand" to="#">USER APP</Link>
+                <div className="ml-auto">
+                    <ul className="navbar-nav">
+                    <li className="nav-list"><Link className="nav-link" to="index">HOME</Link></li>
+                    <li className="nav-list"><Link className="nav-link" to="user">USER</Link></li>
+                    </ul>
                 </div>
         </nav>
     </div>
--- react_UserApp/src/components/UserList.jsx
+++ react_UserApp/src/components/UserList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class UserList extends Component {
+class UserList extends Component {
   render() {
     return (
       <div className='container'>
