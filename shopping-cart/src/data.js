b9da020cb2f17b6e36b9f4b3ab3b2c76d0faca9b
shacz007 <sachinsiddrampur@gmail.com>
2023-04-14 18:27:39 +0530

    ok

---
+++ shopping-cart/src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            name: "sitar",
+            price: 1200,
+            image:"https://www.shutterstock.com/image-vector/beautiful-sitar-classical-music-instrument-260nw-1302006586.jpg"
+        },
+        {
+            id: 2,
+            name: "guitar",
+            price: 1400,
+            image:"https://www.shutterstock.com/image-vector/electric-guitar-flat-vector-illustration-260nw-577465888.jpg"
+        },
+        {
+            id: 3,
+            name: "trumpet",
+            price: 1230,
+            image:"https://image.shutterstock.com/image-photo/trumpet-isolated-260nw-220569841.jpg"
+        },
+    ]
+}
+export default data
