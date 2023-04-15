f88a9ddf06365e65a2c56c5f3af802508d7fe02c
shacz007 <sachinsiddrampur@gmail.com>
2023-04-15 12:15:46 +0530

    ok

--- shopping-cart/src/data.js
+++ shopping-cart/src/data.js
@@ -18,6 +18,18 @@ const data = {
             price: 1230,
             image:"https://image.shutterstock.com/image-photo/trumpet-isolated-260nw-220569841.jpg"
         },
+        {
+            id: 4,
+            name: "flute",
+            price: 140,
+            image:"https://www.shutterstock.com/image-photo/classical-musical-instrument-flute-isolated-260nw-747853216.jpg"
+        },
+        {
+            id: 5,
+            name: "piano",
+            price: 5000,
+            image:"https://www.shutterstock.com/image-illustration/grand-piano-260nw-92365309.jpg"
+        },
     ]
 }
 export default data
